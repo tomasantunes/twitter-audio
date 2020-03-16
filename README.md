425af3400fcfd817089250121cee653b141649c0
Tomás Antunes <tomasantunes@gmail.com>
2020-03-16 14:40:35 +0000

    Create repo

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # twitter-audio
-Convert tweets to audio
+
+### How to run
+
+```
+Set the key and token on https://developer.twitter.com/
+pip install tweepy
+pip install pywin32
+python main.py
+```
+
+[![paypal](https://i.imgur.com/URcFCl1.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Y4DJVGBKXB8MJ&source=url)
+
+[![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/Y8Y2M1UI)
---
+++ main.py
@@ -0,0 +1,63 @@
+import tweepy
+import sys
+import win32com.client as wincl
+
+
+auth = tweepy.OAuthHandler("API_KEY", "API_ACCESS_KEY")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+api = tweepy.API(auth)
+
+count = len(sys.argv)
+global exists
+exists = False
+
+speak = wincl.Dispatch("SAPI.SpVoice")
+
+def cleanTweet(text):
+	stopwords = ('http','rt','@', '#')
+	textwords = text.split()
+
+	resultwords  = [word for word in textwords if not word.lower().startswith(stopwords)]
+	result = ' '.join(resultwords)
+	return result
+
+
+public_tweets = api.home_timeline()
+if (count == 1):
+	for tweet in public_tweets:
+		if (tweet.truncated == "true"):
+			text = tweet["extended_tweet"].encode("utf8")
+			text = cleanTweet(text)
+			exists = True
+			print(text)
+			print("------------------------------------------------")
+			speak.Speak(text)
+		else:
+			text = tweet.text.encode("utf8")
+			text = cleanTweet(text)
+			exists = True
+			print(text)
+			print("------------------------------------------------")
+			speak.Speak(text)
+	if (not exists):
+		print("No results were found")
+elif count == 2:
+	for tweet in tweepy.Cursor(api.user_timeline, screen_name=sys.argv[1], tweet_mode="extended").items():
+		if (tweet['truncated'] == True):
+			text = tweet["extended_tweet"].encode("utf8")
+			text = cleanTweet(text)
+			exists = True
+			print(text)
+			print("------------------------------------------------")
+			speak.Speak(text)
+		else:
+			text = tweet.text.encode("utf8")
+			text = cleanTweet(text)
+			exists = True
+			print(text)
+			print("------------------------------------------------")
+			speak.Speak(text)
+		
+	if (not exists):
+		print("No results were found")
